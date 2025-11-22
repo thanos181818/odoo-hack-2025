a0735607cd8617444a15519382d962e3bfb83a16
uncodingthecode <palash.shah23@spit.ac.in>
2025-11-22 15:14:26 +0530

    chore: remove build output from Git

--- frontend/.next/dev/static/chunks/app_page_tsx_ab2f99fa._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_fb9594d7._.js",
-  "static/chunks/_a715acc5._.js"
-],
-    source: "dynamic"
-});
